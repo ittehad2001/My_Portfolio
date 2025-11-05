a6007f7d9ef5698107b634c68d531dbba315bd39
ittehad2001 <ittehadbinrahim@gmail.com>
2025-11-05 13:41:29 +0600

    cv link added

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -65,7 +65,7 @@ export function Hero() {
               asChild
               className="bg-white text-black hover:bg-white/10 hover:text-white"
             >
-              <a href="https://drive.google.com/file/d/1GTs3E_vQTAIDgpwFRdaR-wscd0Xifd3B/view?usp=sharing" className="group" target="_blank" rel="noopener noreferrer">
+              <a href="https://drive.google.com/file/d/1ImAFJPGWglhds6j65s6m_fU2Ok6MR32S/view?usp=sharing" className="group" target="_blank" rel="noopener noreferrer">
                 <span className="flex items-center justify-between gap-2"><span><File /></span> Resume </span>
               </a>
             </Button>
